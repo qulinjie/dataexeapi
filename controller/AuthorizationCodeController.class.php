8c2f00a8a1519ae927860980edae36992334c5e0
zhangkui <zhangkui@d4452c07-b0bf-42be-a67f-5257330459db>
2015-12-09 02:09:46 +0000

    开发授权码管理功能。增加、停用。

--- controller/AuthorizationCodeController.class.php
+++ controller/AuthorizationCodeController.class.php
@@ -18,6 +18,15 @@ class AuthorizationCodeController extends BaseController {
                 case 'searchList':
                     $this->getSearchList($req_data);
                     break;
+                case 'update':
+                    $this->update($req_data);
+                    break;
+                case 'getInfo':
+                    $this->getInfo($req_data);
+                    break;
+                case 'create':
+                    $this->create($req_data);
+                    break;
                 default:
                     Log::error('page not found . ' . $params[0]);
                     EC::fail(EC_MTD_NON);
@@ -43,6 +52,39 @@ class AuthorizationCodeController extends BaseController {
         $data = $code_model->getSearchList($params, $current_page, $page_count);
     
         EC::success(EC_OK,$data);
+    }
+    
+    public function update($req_data){
+        $id = $req_data['id'];
+        $user_id = $req_data['user_id'];
+        unset($req_data['id']);
+        unset($req_data['user_id']);
+        
+        $code_model = $this->model('authorizationCode');
+        $res = $code_model->updateAuthCode($req_data,array('id' => $id,'user_id' => $user_id));
+        if(false === $res){
+            Log::error('updateAuthCode faild !');
+            EC::fail(EC_UPD_REC);
+        }
+        EC::success(EC_OK);
+    }
+    
+    public function getInfo($req_data){
+        $code_model = $this->model('authorizationCode');
+        $data = $code_model->getInfoAuthCode($req_data, array());
+        EC::success(EC_OK,$data);
+    }
+    
+    public function create($req_data){
+        $id = $this->model('id')->getAuthorizationCodeId();
+        $req_data['id'] = $id;
+        $code_model = $this->model('authorizationCode');
+        $data = $code_model->createAuthCode($req_data);
+        if(false === $data){
+            Log::error('createAuthCode Fail!');
+            EC::fail(EC_ADD_REC);
+        }
+        EC::success(EC_OK,$id);
     }
     
 }
--- model/AuthorizationCodeModel.class.php
+++ model/AuthorizationCodeModel.class.php
@@ -1,29 +1,73 @@
 <?php
-
 class AuthorizationCodeModel extends Model {
     
 	public function tableName(){
 		return 'c_authorization_code';
 	}
 	
+	// 是否删除 1-正常 2-已删除
+	public static $_is_delete_false = 1;
+	public static $_is_delete_true = 2;
+	
+	// 状态 1-正常/启用 2-停用 3-失效
+	public static $_status_enabled = 1;
+	public static $_status_disabled = 2;
+	public static $_status_overdue = 3;
+	
+	// 使用方式 1-按次数 2-按时间段
+	public static $_type_count = 1;
+	public static $_type_time = 2;
+	
+	
 	public function getSearchCnt($params = array()){
 	    
-	    $params['is_delete'] = 1; // 1-正常 2-删除
+	    $keys = array();
+	    $values = array();
 	    
-	    Log::notice('getSearchCnt ==== >>> params=' . json_encode($params) );
-	    return $this->count(null, 'id', $params);
+	    $keys[] = 'is_delete = ?';
+	    $values[] = AuthorizationCodeModel::$_is_delete_false;
+	    
+	    $fields = ['user_id', 'code', 'time1', 'time2','type','status'];
+	    foreach ($fields as $key => $val){
+	        if( !$params[$val] ){
+	            continue;
+	        }
+	        switch ( $val ) {
+	            case 'time1':
+	                $keys[] = "add_timestamp >= ?";
+	                $values[] = $params[$val];
+	                break;
+	            case 'time2':
+	                $keys[] = "add_timestamp <= ?";
+	                $values[] = $params[$val];
+	                break;
+	            default:
+	                $keys[] = "{$val}=?";
+	                $values[] = $params[$val];
+	                break;
+	        }
+	    }
+	    
+	    Log::notice('getSearchCnt ==== >>> keys=' . json_encode($keys) . ',values=' . json_encode($values) );
+	    return $this->count(null, 'id', $keys, $values);
 	}
 	
 	public function getSearchList($params = array(), $page = null, $count = null){
 	    $model = $this->from();
 	    
 	    $where = [];
-	    $fields = ['code','user_id','type','status'];
+	    $fields = ['user_id', 'code', 'time1', 'time2','type','status'];
 	    foreach ($fields as $key => $val){
 	        if( !$params[$val] ){
 	            continue;
 	        }
 	        switch ( $val ) {
+	            case 'time1':
+	                $where[] = "add_timestamp >= '{$params[$val]}'";
+	                break;
+                case 'time2':
+                    $where[] = "add_timestamp <= '{$params[$val]}'";
+                    break;
 	            default:
 	                $where[] = "{$val}='{$params[$val]}'";
 	                break;
@@ -41,5 +85,47 @@ class AuthorizationCodeModel extends Model {
 	    return $model->order('add_timestamp desc')->select();
 	}
 	
+	public function updateAuthCode($param, $where){
+	    if(empty($where)){
+	        Log::error('!!! upate all rows of record .');
+	        return false;
+	    }
+	    if(empty($param)){
+	        Log::error('!!! param is empyt .');
+	        return false;
+	    }
+	    return $this->update($param, $where);
+	}
+	
+	public function getInfoAuthCode($where = array(),$fields = array()){
+	    Log::notice('getInfo ==== >>> where=' . json_encode($where) );
+	    if(empty($fields)){
+	        $data = $this->where($where)->where(array('is_delete'=>AuthorizationCodeModel::$_is_delete_false))->from()->select();
+	    }else{
+	        $data = $this->where($where)->where(array('is_delete'=>AuthorizationCodeModel::$_is_delete_false))->from(null,$fields)->select();
+	    }
+	    return $data;
+	}
+	
+	public function createAuthCode($param = array()){
+	    if(! $this->insert(array(
+	        'id'   =>	$param['id'],
+            'user_id' => $param['user_id'],
+	        'code' => $param['code'],
+	        'type'	=>	$param['type'],
+	        'active_count'	=>	$param['active_count'],
+	        'time_start'	=>	$param['time_start'],
+	        'time_end'	=>	$param['time_end'],
+	        'comment'	=>	$param['comment'],
+            'used_count'	=>	0,
+            'status'	=>	AuthorizationCodeModel::$_status_enabled,
+	        'is_delete'	=>	AuthorizationCodeModel::$_is_delete_false,
+	        'add_timestamp' => date('Y-m-d H:i:s',time())
+	    ))){
+	        Log::error('create record err . ErrorNo=' . $this->getErrorNo() . ' ,ErrorInfo=' . $this->getErrorInfo());
+	        return false;
+	    }
+	    return true;
+	}
 	
 }
